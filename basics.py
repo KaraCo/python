733aa22dea11a5b5de8a08d5a4cd5810a149a5e9
KaraCo <parlando80@hotmail.com>
2017-08-10 12:58:40 -0400

    Update basics.py

--- basics.py
+++ basics.py
@@ -7,3 +7,7 @@ phone = "Pixel"
 welcome_msg = "Hi {0}, congratulations on your new {2} phone.\nWelcome to the future. \nWelcome to {1}".format(name, os, phone)
 
 print(welcome_msg)
+
+# Raw strings
+
+path = r'C:\Users\Bob\Documents\rawpaths.py'

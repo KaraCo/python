ae885ec6698d403f32934a82da1d75be349f7717
KaraCo <parlando80@hotmail.com>
2017-08-10 12:49:32 -0400

    Adding some basic code
    :+1:

---
+++ basics.py
@@ -0,0 +1,9 @@
+# Using Format Function
+
+name = "Husbie"
+os = "Android"
+phone = "Pixel"
+
+welcome_msg = "Hi {0}, congratulations on your new {2} phone.\nWelcome to the future. \nWelcome to {1}".format(name, os, phone)
+
+print(welcome_msg)
